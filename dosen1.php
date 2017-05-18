0accb14d743a8c9c477d21a6167154807c7e3d20
Rendi Sukirman <rendisukirman.rs@gmail.com>
2017-05-19 03:18:05 +0700

    Update and rename .gitignore to dosen1.php

---
+++ dosen1.php
@@ -0,0 +1,124 @@
+<?php
+echo "<h1><center>Selamat Datang Pak Andang</center></h1>";
+echo "<h1><center>Berikut Jadwal Anda</center></h1>";
+echo "<hr>";
+?>
+<?php
+echo "<br>";
+echo "<center>Berikut Jadwal Kuliah Anda Hari ini</center>";
+echo "<br>";
+echo "<center><table width = 500 border =1>
+<tr bgcolor=00fffff>
+<td>Jadwal</td>
+<td>Waktu</td>
+<td>Tempat</td>
+</tr>
+<tr>
+<td>baris kedua</td>
+<td>baris ke empat</td>
+<td>Tempat</td>
+</tr>
+</table></center>";
+?>
+<?php
+echo "<br>";
+echo "<center>Sudah Waktunya Anda mengupload nilai</center>";
+echo "<br>";
+echo "<center><table width = 500 border =1>
+<tr bgcolor=00fffff>
+<td>Jadwal</td>
+<td>Waktu</td>
+<td>Tempat</td>
+</tr>
+<tr>
+<td>baris kedua</td>
+<td>baris ke empat</td>
+<td>Tempat</td>
+</tr>
+</table></center>";
+?>
+<?php
+echo "<br>";
+echo "<center>Jadwal Kuliah Anda Hari ini</center>";
+echo "<br>";
+echo "<center><table width = 500 border =1>
+<tr bgcolor=00fffff>
+<td>Jadwal</td>
+<td>Waktu</td>
+<td>Tempat</td>
+</tr>
+<tr>
+<td>baris kedua</td>
+<td>baris ke empat</td>
+<td>Tempat</td>
+</tr>
+</table></center>";
+?>
+<?php
+echo "<br>";
+echo "<center>Jadwal UAS Anda Hari ini</center>";
+echo "<br>";
+echo "<center><table width = 500 border =1>
+<tr bgcolor=00fffff>
+<td>Jadwal</td>
+<td>Waktu</td>
+<td>Tempat</td>
+</tr>
+<tr>
+<td>baris kedua</td>
+<td>baris ke empat</td>
+<td>Tempat</td>
+</tr>
+</table></center>";
+?>
+<?php
+echo "<br>";
+echo "<center>Jadwal UTS Anda Hari ini</center>";
+echo "<br>";
+echo "<center><table width = 500 border =1>
+<tr bgcolor=00fffff>
+<td>Jadwal</td>
+<td>Waktu</td>
+<td>Tempat</td>
+</tr>
+<tr>
+<td>baris kedua</td>
+<td>baris ke empat</td>
+<td>Tempat</td>
+</tr>
+</table></center>";
+?>
+<?php
+echo "<br>";
+echo "<center>Jadwal Upload Nilai UTS Anda Hari ini</center>";
+echo "<br>";
+echo "<center><table width = 500 border =1>
+<tr bgcolor=00fffff>
+<td>Jadwal</td>
+<td>Waktu</td>
+<td>Tempat</td>
+</tr>
+<tr>
+<td>baris kedua</td>
+<td>baris ke empat</td>
+<td>Tempat</td>
+</tr>
+</table></center>";
+?>
+<?php
+echo "<br>";
+echo "<center>Jadwal Upload Nilai UAS Anda Hari ini</center>";
+echo "<br>";
+echo "<center><table width = 500 border =1>
+<tr bgcolor=00fffff>
+<td>Jadwal</td>
+<td>Waktu</td>
+<td>Tempat</td>
+</tr>
+<tr>
+<td>baris kedua</td>
+<td>baris ke empat</td>
+<td>Tempat</td>
+</tr>
+</table></center>";
+?>
